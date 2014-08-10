19140331e1806d590a4962b0093cbc9f82a02b0e
gatsby-ice-type-8080 <gatsby.ice.type.8080@gmail.com>
2014-08-10 14:54:55 +0900

    ナビゲーションバーを追加 新規登録をナビゲーションバーに移動

--- src/main/ts/app.ts
+++ src/main/ts/app.ts
@@ -26,11 +26,17 @@ angular
         
         $httpProvider.interceptors.push('HttpResponseInterceptor');
     }])
-.run(() => {
-    var validator = (<any>$).validator;
-    
-    _.extend(validator.messages, {
-        required: 'この項目は必須入力です。',
-        maxlength: validator.format('この項目は {0} 文字以下で入力してください。')
-    });
-});
+.run(['$rootScope', '$location', '$route',
+    ($rootScope, $location, $route) => {
+        var validator = (<any>$).validator;
+        
+        _.extend(validator.messages, {
+            required: 'この項目は必須入力です。',
+            maxlength: validator.format('この項目は {0} 文字以下で入力してください。')
+        });
+        
+        $rootScope.moveToTopPage = () => {
+            $location.path('/');
+            $route.reload(); // 現在のページが "/" の場合、これをしないと再描画してくれない
+        };
+    }]);
