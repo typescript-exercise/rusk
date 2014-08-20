ce0ea110d6187be20565e82d5b1fdbc1c9e2ce7c
gatsby-ice-type-8080 <gatsby.ice.type.8080@gmail.com>
2014-08-20 21:59:52 +0900

    TypeScriptリファクタリング 詳細ページのフォーム作成を Form クラスに移動

--- src/main/ts/rusk/controller/task/TaskDetailController.ts
+++ src/main/ts/rusk/controller/task/TaskDetailController.ts
@@ -10,71 +10,75 @@ angular
                 $scope.task = task;
             });
         
-        $scope.init = () => {
-            form = createTaskDetailForm($scope);
-        };
-        
-        $scope.remove = () => {
-            removeTaskService.remove({
-                id: $scope.task.id,
-                title: $scope.task.title,
-                onRemove: () => {
-                    $location.path('/');
-                }
-            });
-        };
-        
-        $scope.modify = () => {
-            if (!form.isValid()) {
-                return;
-            }
+        $.extend($scope, {
+            init: () => {
+                appendCurrentTaskStatusForPulldown($scope);
+                form = createTaskDetailForm($scope);
+            },
             
-            var params = _.extend({
-                              id: $scope.task.id,
-                              lastUpdateDate: $scope.task.updateDate
-                          }, form.getParams());
-            
-            taskResource.modify(params, 
-                function onSuccess() {
-                    toastr.success('「' + $scope.task.title + '」を更新しました。');
-                    $location.path('/');
-                },
-                function onNotFoundError() {
-                    alert('このタスクは既に削除されています。');
+            remove: () => {
+                removeTaskService.remove({
+                    id: $scope.task.id,
+                    title: $scope.task.title,
+                    onRemove: () => {
+                        $location.path('/');
+                    }
                 });
-        };
+            },
+            
+            modify: () => {
+                if (!form.isValid()) {
+                    return;
+                }
+                
+                var params = _.extend({
+                                  id: $scope.task.id,
+                                  lastUpdateDate: $scope.task.updateDate
+                              }, form.getParams());
+                
+                taskResource.modify(params, 
+                    function onSuccess() {
+                        toastr.success('「' + $scope.task.title + '」を更新しました。');
+                        $location.path('/');
+                    },
+                    function onNotFoundError() {
+                        alert('このタスクは既に削除されています。');
+                    });
+            }
+        });
         
-        function createTaskDetailForm($scope) : rusk.view.form.TaskDetailForm {
-            
-            var title = new rusk.view.primitive.TextBox($('#title'), new rusk.view.primitive.AngularScopeValue($scope.task, 'title'));
-            
-            appendCurrentTaskStatus($scope);
-            var status = new rusk.view.primitive.SelectBox(new rusk.view.primitive.AngularScopeValue($scope.task, 'status'));
-            
-            var period = new rusk.view.primitive.DateTime($('#period'), {
-                defaultDate: $scope.task.priority.urgency.period,
-                minDate: $scope.task.registeredDate
-            });
-            
-            var importance = new rusk.view.primitive.SelectBox(new rusk.view.primitive.AngularScopeValue($scope.task.priority, 'importance'));
-            
-            var detail = new rusk.view.primitive.TextArea($('#detail'), new rusk.view.primitive.AngularScopeValue($scope.task, 'detail'));
-            
-            var validateOptions = rusk.view.form.TaskValidateOptionBuilder.create().title().period().importance().detail().build();
-            var form = new rusk.view.primitive.Form($('#task-detail'), validateOptions);
-            
-            return new rusk.view.form.TaskDetailForm({
-                title: title,
-                status: status,
-                period: period,
-                importance: importance,
-                detail: detail,
-                form: form
-            });
+        function appendCurrentTaskStatusForPulldown($scope) {
+            $scope.task.enableToSwitchStatusList.unshift($scope.task.status);
         }
         
-        function appendCurrentTaskStatus($scope) {
-            $scope.task.enableToSwitchStatusList.unshift($scope.task.status);
+        function createTaskDetailForm($scope) : rusk.view.form.TaskDetailForm {
+            return new rusk.view.form.TaskDetailForm({
+                task: $scope.task,
+                title: {
+                    selector: '#title',
+                    name: 'title'
+                },
+                status: {
+                    name: 'status'
+                },
+                period: {
+                    selector: '#period',
+                    options: {
+                        defaultDate: $scope.task.priority.urgency.period,
+                        minDate: $scope.task.registeredDate
+                    }
+                },
+                importance: {
+                    name: 'importance'
+                },
+                detail: {
+                    selector: '#detail',
+                    name: 'detail'
+                },
+                form: {
+                    selector: '#task-detail'
+                }
+            });
         }
     }
 ]);
--- src/main/ts/rusk/view/form/TaskDetailForm.ts
+++ src/main/ts/rusk/view/form/TaskDetailForm.ts
@@ -1,6 +1,35 @@
 module rusk {
     export module view {
         export module form {
+            
+            export interface TaskDetailFormConstructParameter {
+                task: any;
+                title: {
+                    selector: string;
+                    name: string;
+                }
+                status: {
+                    name: string;
+                }
+                period: {
+                    selector: string;
+                    options: {
+                        defaultDate: any;
+                        minDate: any;
+                    }
+                }
+                importance: {
+                    name: string;
+                }
+                detail: {
+                    selector: string;
+                    name: string;
+                }
+                form: {
+                    selector: string;
+                }
+            }
+            
             export class TaskDetailForm {
                 private title : primitive.TextBox;
                 private status : primitive.SelectBox;
@@ -9,13 +38,50 @@ module rusk {
                 private detail : primitive.TextArea;
                 private form : primitive.Form;
                 
-                constructor(formElements) {
-                    this.title = formElements.title;
-                    this.status = formElements.status;
-                    this.period = formElements.period;
-                    this.importance = formElements.importance;
-                    this.detail = formElements.detail;
-                    this.form = formElements.form;
+                constructor(param : TaskDetailFormConstructParameter) {
+                    this.initTitle(param);
+                    this.initStatus(param);
+                    this.initPeriod(param);
+                    this.initImportance(param);
+                    this.initDetail(param);
+                    this.initForm(param);
+                }
+                
+                private initTitle(param : TaskDetailFormConstructParameter) {
+                    var $element = $(param.title.selector);
+                    var accessor = this.createAccessor(param.task, param.title.name);
+                    this.title = new primitive.TextBox($element, accessor);
+                }
+                
+                private initStatus(param : TaskDetailFormConstructParameter) {
+                    var accessor = this.createAccessor(param.task, param.status.name);
+                    this.status = new primitive.SelectBox(accessor);
+                }
+                
+                private initPeriod(param : TaskDetailFormConstructParameter) {
+                    var $element = $(param.period.selector);
+                    this.period = new primitive.DateTime($element, param.period.options);
+                }
+                
+                private initImportance(param : TaskDetailFormConstructParameter) {
+                    var accessor = this.createAccessor(param.task.priority, param.importance.name);
+                    this.importance = new primitive.SelectBox(accessor);
+                }
+                
+                private initDetail(param : TaskDetailFormConstructParameter) {
+                    var $element = $(param.detail.selector);
+                    var accessor = this.createAccessor(param.task, param.detail.name);
+                    this.detail = new primitive.TextArea($element, accessor);
+                }
+                
+                private initForm(param : TaskDetailFormConstructParameter) {
+                    var $element = $(param.form.selector);
+                    var validateOptions = TaskValidateOptionBuilder.create().title().period().importance().detail().build();
+                    this.form = new primitive.Form($element, validateOptions);
+                }
+                
+                private createAccessor(scope, name) : primitive.AngularScopeValue {
+                    return new primitive.AngularScopeValue(scope, name)
                 }
                 
                 isValid() : boolean {
